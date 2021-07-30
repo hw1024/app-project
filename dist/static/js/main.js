f564735ac765a469de665b594c2cf46ca1b87ff9
weihu <weihu@deloitte.com.cn>
2021-07-30 14:22:34 +0800

    1

---
+++ dist/static/js/main.js
@@ -0,0 +1,133 @@
+$(document).ready(function () {
+    // 首页动画
+    $('.page1 .section-img1').fadeThis({
+        speed: 600,
+    });
+    $('.page1 .fade1').fadeThis({
+        speed: 1500,
+        distance: 200
+    });
+    $('.page1 .fade2').fadeThis({
+        speed: 1500,
+        distance: 350
+    });
+    $('.page1 .fade3').fadeThis({
+        speed: 1500,
+        distance: 500
+    });
+    $('.page1 .section-img2').fadeThis({
+        speed: 600,
+    });
+    $('.page1 .fade4').fadeThis({
+        speed: 1500,
+        distance: 200
+    });
+    $('.page1 .fade5').fadeThis({
+        speed: 1500,
+        distance: 350
+    });
+
+    $('.page2 .section-img1').fadeThis({
+        speed: 600,
+    });
+    $('.page2 .fade1').fadeThis({
+        speed: 1500,
+        distance: 200
+    });
+    $('.page2 .fade2').fadeThis({
+        speed: 1500,
+        distance: 350
+    });
+    $('.page2 .section-img2').fadeThis({
+        speed: 600,
+    });
+    $('.page2 .section-problem').fadeThis({
+        speed: 600,
+    });
+    $('.page2 .section-img-icon1').fadeThis({
+        speed: 800,
+    });
+    $('.page2 .section-img-icon2').fadeThis({
+        speed: 800,
+    });
+    $('.page2 .section-img-icon3').fadeThis({
+        speed: 800,
+    });
+    $('.page2 .section-img-icon4').fadeThis({
+        speed: 1000,
+    });
+    $('.page2 .section-img-icon5').fadeThis({
+        speed: 1200,
+    });
+    $('.page2 .section-img-icon6').fadeThis({
+        speed: 1200,
+    });
+
+    $('.page2 .fade3').fadeThis({
+        speed: 1500,
+        distance: 200
+    });
+    $('.page2 .fade4').fadeThis({
+        speed: 1500,
+        distance: 350
+    });
+    $('.page2 .fade5').fadeThis({
+        speed: 600,
+    });
+
+    $('.page3 .title').fadeThis({
+        speed: 600,
+    });
+    $('.page3 .section-img').fadeThis({
+        speed: 900,
+        distance: 100
+    });
+    $('.page3 .section-img1').fadeThis({
+        speed: 1100,
+        distance: 100
+    });
+    $('.page3 .section-img2').fadeThis({
+        speed: 1300,
+        distance: 100
+    });
+    $('.page3 .section-img3').fadeThis({
+        speed: 1500,
+        distance: 100
+    });
+    $('.page3 .section-img4').fadeThis({
+        speed: 1700,
+        distance: 100
+    });
+    $('.page3 .section-img5').fadeThis({
+        speed: 2500,
+        distance: 100
+    });
+    $('.page3 .section-text').fadeThis({
+        speed: 900,
+        distance: 100
+    });
+    $('.page3 .section-text1').fadeThis({
+        speed: 1100,
+        distance: 100
+    });
+    $('.page3 .section-text2').fadeThis({
+        speed: 1300,
+        distance: 100
+    });
+    $('.page3 .section-text3').fadeThis({
+        speed: 1700,
+        distance: 100
+    });
+    $('.page3 .section-text4').fadeThis({
+        speed: 2100,
+        distance: 100
+    });
+    $('.page3 .section-text5').fadeThis({
+        speed: 2500,
+        distance: 100
+    });
+
+    $(window).fadeThis({
+        speed: 1000,
+    });
+});
